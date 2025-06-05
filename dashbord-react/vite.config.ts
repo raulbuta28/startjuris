49ffb1cc1b691a4ce77f14b1d3ae003c0d610a55
StartJuris <raulbuta28@gmail.com>
2025-06-05 11:15:52 +0300

    Configure React dev server to proxy API

--- dashbord-react/vite.config.ts
+++ dashbord-react/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
   }
 });
